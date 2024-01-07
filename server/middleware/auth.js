f04e7fc34bc1561237dd453f796ec759b501159e
Prateek <prateek.kumar.original@gmail.com>
2024-01-07 22:09:25 +0530

    Create auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,4 +1,42 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const SECRET = process.env.SECRET_KEY;
+const SECRET = process.env.SECRET;
+
+module.exports.authenticateUser = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            if (user.role !== 'user') {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+module.exports.authenticateInstructor = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, SECRET, (err, instructor) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            if (instructor.role !== 'instructor') {
+                return res.sendStatus(403);
+            }
+            req.instructor = instructor;
+            next();
+        });
+    } else {
+        res.sendStatus(403);
+    }
+}
