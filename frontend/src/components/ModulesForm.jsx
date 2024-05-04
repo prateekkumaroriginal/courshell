1c96784169f839048323eafb0729f158947f13dc
Prateek <prateek.kumar.original@gmail.com>
2024-05-05 01:21:16 +0530

    Add Module creation functionality from course page and module reorder functionality.

---
+++ frontend/src/components/ModulesForm.jsx
@@ -0,0 +1,127 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import ModulesList from './ModulesList';
+
+const formSchema = z.object({
+    title: z.string().min(4).max(200)
+});
+
+const ModulesForm = ({ course, courseId, fetchData }) => {
+    const [isCreating, setIsCreating] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    const toggleCreating = () => {
+        setIsCreating(curr => !curr);
+    }
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+        },
+    }, []);
+
+    const { handleSubmit, reset, formState: { isSubmitting, isValid } } = form;
+
+    const onSubmit = async (values) => {
+        try {
+            console.log(values);
+            const response = await fetch(`http://localhost:3000/instructor/courses/${courseId}/modules`, {
+                method: 'POST',
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('token')}`,
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            });
+            setIsCreating(false);
+            reset(form);
+            fetchData();
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const onReorder = async (updateData) => {
+        try {
+            setIsUpdating(true);
+            await fetch(`http://localhost:3000/instructor/courses/${courseId}/modules/reorder`, {
+                method: 'PUT',
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('token')}`,
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    list: updateData
+                })
+            });
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setIsUpdating(false);
+        }
+    }
+
+    return (
+        <div className='mt-6 border bg-slate-200 rounded-md p-4'>
+            <div className='font-medium flex items-center justify-between'>
+                <p className='font-bold text-zinc-600 pt-2 pb-4'>Modules</p>
+                {isCreating ? <div className='flex gap-x-2'>
+                    <button
+                        className='px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md'
+                        onClick={() => {
+                            setIsCreating(false)
+                            reset(form)
+                        }}>
+                        Cancel
+                    </button>
+                    <button
+                        type='submit'
+                        disabled={!isValid || isSubmitting}
+                        onClick={handleSubmit(onSubmit)}
+                        className='px-4 py-2 bg-gray-600 hover:bg-gray-700 text-white rounded-md'>
+                        Create
+                    </button>
+                </div> : <button onClick={() => {
+                    setIsCreating(true)
+                }}>
+                    ➕ Add a Module
+                </button>}
+            </div>
+
+            {isCreating ? <form
+                className='mt-2'
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <input
+                    className='p-1 shadow-lg appearance-none rounded w-full outline-none'
+                    type="text"
+                    id='title'
+                    placeholder="e.g. 'Introduction to the course'"
+                    disabled={isSubmitting}
+                    {...form.register('title')}
+                />
+            </form> : <>
+                <div className={!course.modules.length ? 'text-zinc-600 text-sm font-semibold mt-2 italic' : 'mt-2'}>
+                    {!course.modules.length && "No Modules"}
+                    <ModulesList
+                        onEdit={() => { }}
+                        onReorder={onReorder}
+                        items={course.modules || []}
+                    />
+                </div>
+                <div>
+                    {course.modules.length !== 0 && <p className='text-xs text-slate-600 mt-4'>
+                        Drag and drop to reorder modules
+                    </p>}
+                </div>
+            </>}
+
+
+        </div>
+    )
+}
+
+export default ModulesForm
---
+++ frontend/src/components/ModulesList.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import {
+    DragDropContext,
+    Draggable,
+    Droppable,
+} from "@hello-pangea/dnd";
+import { Grip, Pencil } from "lucide-react";
+
+const ModulesList = ({ items, onReorder, onEdit }) => {
+    const [isMounted, setIsMounted] = useState(false);
+    const [modules, setModules] = useState(items);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    useEffect(() => {
+        setModules(items);
+    }, [items]);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    const onDragEnd = (result) => {
+        if (!result.destination) {
+            return;
+        }
+
+        const items = Array.from(modules);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        setModules(items);
+
+        const startIndex = Math.min(result.source.index, result.destination.index);
+        const endIndex = Math.max(result.source.index, result.destination.index);
+        const updatedModules = items.slice(startIndex, endIndex + 1);
+
+        const bulkUpdateData = updatedModules.map(module => ({
+            _id: module._id,
+            position: items.findIndex(item => item._id === module._id)
+        }));
+
+        onReorder(bulkUpdateData);
+    }
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId='modules'>
+                {(provided) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                        {modules.map((module, index) => {
+                            const published = module.articles.some(article => article.published);
+
+                            return (
+                                <Draggable
+                                    key={module._id}
+                                    draggableId={module._id}
+                                    index={index}
+                                >
+                                    {(provided) => (
+                                        <div
+                                            className={`flex items-center gap-x-2 bg-slate-300 border-slate-200 border text-slate-700 rounded-md mb-2 text-sm ${published && "bg-sky-100 border-sky-200 text-sky-700"}`}
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                        >
+                                            <div
+                                                className={`px-2 py-3 border-r border-r-slate-200 hover:bg-slate-400 active:bg-slate-400 rounded-l-md transition ${published && "hover:bg-sky-200 border-r-sky-200"}`}
+                                                {...provided.dragHandleProps}
+                                            >
+                                                <Grip className='h-5 w-5' />
+                                            </div>
+                                            {module.title}
+                                            <div className='ml-auto pr-2 flex items-center gap-x-2'>
+                                                <div
+                                                    className={`text-white px-2 py-1 font-semibold rounded-full text-xs cursor-default ${published ? "bg-sky-700 hover:bg-sky-800" : "bg-slate-600 hover:bg-slate-700"}`}
+                                                >
+                                                    {published ? "Published" : "Draft"}
+                                                </div>
+                                                <Pencil
+                                                    className='w-4 h-4 cursor-pointer hover:opacity-75 transition'
+                                                    onClick={() => onEdit(module._id)}
+                                                />
+                                            </div>
+                                        </div>
+                                    )}
+                                </Draggable>
+                            )
+                        })}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    )
+}
+
+export default ModulesList
