4297a4ff290fb95f73ad69185801a6afb2d2bf49
Prateek <prateek.kumar.original@gmail.com>
2024-05-21 12:06:26 +0530

    Make the Input more flexible

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 
-const Input = ({ type, name, label, register }) => {
+const Input = ({ type, name, label, register, defaultValue, className }) => {
     const placeholderMap = {
-        text: "Full Stack",
+        text: "e.g. \"Full Stack\"",
         password: "••••••••",
         email: "johndoe@example.com",
     }
 
     return (
-        <div className='mb-4'>
-            <div className="block">
+        <div>
+            {label && <div className="block">
                 <label className="text-gray-700 text-xl font-bold mr-4" htmlFor={name}>{label}</label>
-            </div>
-            <input className='p-2 shadow-lg outline-none rounded w-full focus:ring-4 focus:ring-blue-600/40'
+            </div>}
+            <input
+                className={className || 'p-2 shadow-lg outline-none rounded w-full focus:ring-4 focus:ring-blue-600/40'}
                 type={type}
                 id={name}
                 {...register(name)}
                 placeholder={placeholderMap[type]}
+                defaultValue={defaultValue || ""}
             />
         </div>
     )
