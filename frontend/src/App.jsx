77c5c422827b15a260728e87095db93d34cbb130
Prateek <prateek.kumar.original@gmail.com>
2024-05-23 23:58:40 +0530

    Add Module Page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Create from './components/Create';
-import Course from './components/Course';
-import Signin from './components/Signin';
-import Navbar from './components/Navbar';
+import Create from '@/components/Create';
+import Course from '@/components/Course';
+import Signin from '@/components/Signin';
+import Navbar from '@/components/Navbar';
+import Module from '@/components/Module';
 
 function App() {
     return (
@@ -13,9 +14,9 @@ function App() {
                 <Route path='/signin' element={<Signin />} />
                 <Route path='/create' element={<Create />} />
                 <Route path='/courses/:courseId' element={<Course />} />
+                <Route path='/courses/:courseId/:moduleId' element={<Module />} />
                 {/* <Route path='/dashboard' element={<Dashboard />} />
                 <Route path='/courses' element={<Courses />} />
-                <Route path='/courses/:courseId/:moduleId' element={<Module />} />
                 <Route path='/courses/:courseId/:moduleId/:articleId' element={<Article />} /> */}
             </Routes>
         </Router>
