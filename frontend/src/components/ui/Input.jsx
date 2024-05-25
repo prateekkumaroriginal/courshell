a289886e631fbfad6a199415157b2890c760b6de
Prateek <prateek.kumar.original@gmail.com>
2024-05-25 21:42:32 +0530

    Modify Input

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Input = ({ type, name, label, register, defaultValue, className }) => {
+const Input = ({ type, name, label, register, defaultValue, className, placeholder }) => {
     const placeholderMap = {
         text: "e.g. \"Full Stack\"",
         password: "••••••••",
@@ -18,14 +18,14 @@ const Input = ({ type, name, label, register, defaultValue, className }) => {
                 type={type}
                 id={name}
                 {...register(name)}
-                placeholder={placeholderMap[type]}
+                placeholder={placeholder || placeholderMap[type]}
                 defaultValue={defaultValue || ""}
             /> : <textarea
                 className={className || 'p-2 min-h-16 shadow-lg outline-none rounded w-full focus:ring-4 focus:ring-blue-600/40'}
                 type={type}
                 id={name}
                 {...register(name)}
-                placeholder={placeholderMap[type]}
+                placeholder={placeholder || placeholderMap[type]}
                 defaultValue={defaultValue || ""}
             ></textarea>}
         </div>
