5f5119447b1e3147356036d3488f5756a46dc38e
Prateek <prateek.kumar.original@gmail.com>
2024-06-03 11:32:07 +0530

    Update Banner

--- frontend/src/components/Instructor/Course.jsx
+++ frontend/src/components/Instructor/Course.jsx
@@ -88,13 +88,13 @@ const Course = () => {
     };
 
     return (
-        <div className='md:p-6'>
+        <div className='md:px-6'>
             <ToastProvider />
             {!isLoading && !isPublished && <Banner
                 label={"This course is unpublished. It will not be visible to the students."}
             />}
 
-            <div className='flex items-center w-full gap-x-8'>
+            <div className='flex items-center w-full gap-x-8 mt-6'>
                 <div className="flex flex-col">
                     <h1 className='text-3xl font-semibold'>
                         Course Settings
--- frontend/src/components/ui/Banner.jsx
+++ frontend/src/components/ui/Banner.jsx
@@ -7,15 +7,15 @@ const iconMap = {
 }
 
 const styleMap = {
-    WARNING: 'bg-yellow-200/75 border-yellow-30 text-primary',
-    SUCCESS: 'bg-emerald-700 border-emerald-800 text-secondary'
+    WARNING: 'bg-yellow-200/75 text-primary',
+    SUCCESS: 'bg-emerald-600 text-secondary'
 }
 
 const Banner = ({ label, variant }) => {
     const Icon = iconMap[variant || "WARNING"];
 
     return (
-        <div className={`flex items-center py-4 mb-2 md:-mt-6 md:-mx-6 ${styleMap[variant || "WARNING"]}`}>
+        <div className={`flex items-center py-4 justify-center ${styleMap[variant || "WARNING"]}`}>
             <div className='flex mx-auto gap-x-2'>
                 <Icon className='h-6 w-6' />
                 {label}
