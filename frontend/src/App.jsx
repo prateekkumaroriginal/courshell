1066b020edcd22a40e8500fffbb0bd1387d0f2ce
Prateek <prateek.kumar.original@gmail.com>
2024-08-13 10:55:00 +0530

    Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import Create from '@/components/Instructor/Create';
 import Course from '@/components/Instructor/Course';
