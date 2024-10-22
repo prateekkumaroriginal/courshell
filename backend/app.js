d38e6a3d6fd5ceae4f25a09981d458f87c08ddf2
Prateek Kumar <prateek.kumar.original@gmail.com>
2024-10-22 23:21:31 +0530

    For deploying on vercel

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import instructorRouter from './routes/instructor.js';
+import userRouter from './routes/user.js';
+import adminRouter from './routes/admin.js';
+import superadminRouter from './routes/superadmin.js';
+import 'dotenv/config';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/instructor", instructorRouter);
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/superadmin", superadminRouter);
+
+export default app;
