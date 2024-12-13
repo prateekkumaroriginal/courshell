d10c14528ff2a698f7ad8040f276aeb32c9e9d9a
Prateek Kumar <prateek.kumar.original@gmail.com>
2024-12-13 20:47:09 +0530

    Add PrivacyPolicy

---
+++ frontend/src/components/common/PrivacyPolicy.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const PrivacyPolicy = () => {
+  return (
+    <div className='p-6 space-y-2'>
+      <h1 className='text-3xl'>
+        Privacy Policy
+      </h1>
+
+
+      At Courshell, we prioritise the privacy of our users. This Privacy Policy outlines how we collect, use, and protect your personal information.
+
+      <div className='text-lg'>
+        1. Information We Collect
+      </div>
+      We collect personal information when you register, interact with our services, or communicate with us. This may include your name, email address, and activity data within the platform.
+
+      <div className='text-lg'>
+        2. How We Use Your Information
+      </div>
+      Your information is used to:
+
+      <ul>
+        <li>Provide and improve our services.</li>
+        <li>Communicate updates, respond to queries, and enhance user experience.</li>
+        <li>Ensure security and compliance with legal obligations.</li>
+      </ul>
+
+      <div className='text-lg'>
+        3. Sharing Your Information
+      </div>
+      We do not sell or share your personal data with third parties except when required by law or necessary for service delivery (e.g., payment processing).
+
+
+      <div className='text-lg'>
+        4. Data Security
+      </div>
+      We implement security measures to protect your information from unauthorised access or misuse. However, no method of electronic transmission or storage is completely secure.
+
+      <div className='text-lg'>
+        5. Your Rights
+      </div>
+      You have the right to access, update, or delete your personal information.
+
+      <div className='text-lg'>
+        6. Updates to This Policy
+      </div>
+      We may update this policy from time to time. Significant changes will be communicated through our platform or via email.
+    </div>
+  )
+}
+
+export default PrivacyPolicy
