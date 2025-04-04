221512fbb9d1daa1ca8af8e07e6b091084a5a909
Prateek Kumar <prateek.kumar.original@gmail.com>
2025-04-04 10:58:32 +0530

    temp push

---
+++ backend/lib/payment-service.js
@@ -0,0 +1,8 @@
+export class PaymentService {
+  constructor() {
+    this.razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+  }
+}
